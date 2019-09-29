b0687de817cd9680f0c658d8b118bfcbac9a208a
stanislav <luckyvegas888@gmail.com>
2019-09-30 00:53:23 +0300

    add serializable to screen

--- hallschemelibrary/src/main/java/com/itrocket/hallschemelibrary/screen/Screen.kt
+++ hallschemelibrary/src/main/java/com/itrocket/hallschemelibrary/screen/Screen.kt
@@ -1,7 +1,10 @@
 package com.itrocket.hallschemelibrary.screen
 
+import java.io.Serializable
+
 data class Screen(
     val backgroundColorInt: Int,
-    val textColorInt : Int,
-    val textSize : Float,
-    val text: String)
+    val textColorInt: Int,
+    val textSize: Float,
+    val text: String
+) : Serializable
